27c8612773df0fb6218ef90bd42cb8a703531af0
cxapython <1598828268@qq.com>
2024-02-29 00:26:30 +0800

    update

--- app/src/main/java/com/example/nativesqlite/MainActivity.kt
+++ app/src/main/java/com/example/nativesqlite/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.nativesqlite
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.nativesqlite.MessageStorage.database
 import com.example.nativesqlite.SQLite.SQLiteCursor
 import com.example.nativesqlite.SQLite.SQLiteDatabase
+import com.example.nativesqlite.SQLite.SQLitePreparedStatement
 import com.example.nativesqlite.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val TABLE_NAME = "user"
+    fun createTables(db: SQLiteDatabase) {
+
+
+        //如果存在user_info表，则删除该表
+        val drop_sql = "DROP TABLE IF EXISTS ${TABLE_NAME};"
+        db.executeFast(drop_sql).stepThis().dispose();
+        //创建user_info表
+        //创建user_info表
+        val create_sql = ("CREATE TABLE IF NOT EXISTS ${TABLE_NAME} ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "name VARCHAR NOT NULL,"
+                + "age INTEGER NOT NULL,"
+                + "height INTEGER NOT NULL,"
+                + "weight DECIMAL(10,2) NOT NULL,"
+                + "married INTEGER NOT NULL,"
+                + "update_time VARCHAR NOT NULL"
+                + ");")
+        db.executeFast(create_sql).stepThis().dispose();
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +48,16 @@ class MainActivity : AppCompatActivity() {
 //        helper.insert(userInfo)
         // Example of a call to a native method
         val database = SQLiteDatabase("/data/data/com.example.nativesqlite/databases/user.db")
+//插入操作示例：待确认
+//        val state: SQLitePreparedStatement = database.executeFast("INSERT INTO user VALUES (?, ?, ?, ?,?,?)");
+//        state.requery();
+//        state.bindInteger(0,3)
+//        state.bindString(1,"xiaohong")
+//        state.bindInteger(2,30)
+//        state.bindLong(3,30L)
+//        state.bindDouble(4,30.0)
+//        state.step();
+//        state.dispose();
         val cursor: SQLiteCursor = database.queryFinalized("SELECT name,age FROM user_info")
         if (cursor.next()) {
             val name = cursor.stringValue(0)
--- app/src/main/java/com/example/nativesqlite/SQLite/SQLiteDatabase.java
+++ app/src/main/java/com/example/nativesqlite/SQLite/SQLiteDatabase.java
@@ -27,6 +27,7 @@ public class SQLiteDatabase {
         isOpen = true;
     }
 
+
     public boolean tableExists(String tableName) throws SQLiteException {
         checkOpened();
         String s = "SELECT rowid FROM sqlite_master WHERE type='table' AND name=?;";
